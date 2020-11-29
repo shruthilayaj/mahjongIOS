05f6df5f27ba797070222b516787c9247a237d36
Shruthilaya Jaganathan <s3jaganathan@gmail.com>
2020-11-29 08:50:56 +0530

    Added encryption to the card file

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+
+plugin 'cocoapods-keys', {
+  :project => "Eidolon",
+  :keys => [
+    "CardSecret"
+  ]}
+
+platform :ios, '9.0'
+
+target 'mahjongiOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mahjongiOS
+
+end
---
+++ mahjongiOS/Encryption/EncryptFile.playground/Contents.swift
@@ -0,0 +1,29 @@
+import PlaygroundSupport
+
+func encryptDecrypt(_ input: String) -> String {
+    let key = [UInt8]("password".utf8) //Can't access Keys framework in playgrounds
+    let input_ = [UInt8](input.utf8)
+    let length = key.count
+    var output = ""
+
+    for i in input_.enumerated() {
+        let byte = [i.element ^ key[i.offset % length]]
+        output.append(String(bytes: byte, encoding: .utf8)!)
+    }
+
+    return output
+}
+
+let fileURL = playgroundSharedDataDirectory.appendingPathComponent("Card2020.txt")
+do {
+    var fileContent = try String(contentsOf: fileURL)
+    fileContent = encryptDecrypt(fileContent)
+    let newFileUrl = playgroundSharedDataDirectory.appendingPathComponent("Card2020Encrypted.txt")
+    do {
+        try fileContent.write(to: newFileUrl, atomically: true, encoding: .utf8)
+    } catch {
+        print("Error writing: \(error)")
+    }
+} catch {
+    print("Error writing: \(error)")
+}
